f3ff386ca18b9a7082f9ee39e4281548124b713c
David Dobervich <david.dobervich+apcsgithub@gmail.com>
2020-04-17 06:27:34 -0700

    update comments

--- RunGraphicalGame.java
+++ RunGraphicalGame.java
@@ -14,15 +14,15 @@ public class RunGraphicalGame extends PApplet {
 
 		// Create the display
 		// parameters: (10,10) is upper left of display
-		// (300, 300) is the width and height
+		// (400, 400) is the width and height
 		display = new Display(this, 10, 10, 400, 400);
 
 		display.setColor(1, color(255, 0, 0)); // SET COLORS FOR PLAYER 1 & 2
 		display.setColor(2, color(0, 255, 0));
 
 		// You can use images instead if you'd like.
-		// d.setImage(1, "c:/data/ball.jpg");
-		// d.setImage(2, "c:/data/cone.jpg");
+		// display.setImage(1, "assets/on.png");
+		// display.setImage(2, "assets/off.png");
 
 		display.initializeWithGame(game);
 	}
@@ -32,7 +32,6 @@ public class RunGraphicalGame extends PApplet {
 		background(200);
 
 		display.drawGrid(game.getGrid()); // display the game
-
 	}
 
 	public void mouseClicked() {
